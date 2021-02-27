f3683d513730d69d186458e06c1a675de3ac13e7
aidenerdogan <ahmeterdogan@zack-ai.com>
2021-02-27 04:26:51 +0300

    NLU code updated v001

--- nlp.py
+++ nlp.py
@@ -36,3 +36,55 @@ for intent in intents['intents']:
 # print("words: ",words)
 # print("classes: ",classes)
 # print("documents: ",documents)
+
+# lemmatization: converting a word to its base form
+# ‘Caring’ -> Lemmatization -> ‘Care’
+# ‘Caring’ -> Stemming -> ‘Car’
+# note: not usin POS tagging
+# lemmatize and lower each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+# sort classes
+classes = sorted(list(set(classes)))
+
+# checking results by printing: while needed you can uncomment print lines
+# documents = combination between patterns and intents
+# print(len(documents), "documents\n",documents)
+# print("==============================\n")
+# classes = intents
+# print(len(classes), "classes", classes)
+# print("==============================\n")
+# words = all words, vocabulary
+# print(len(words), "unique lemmatized words", words)
+
+# pickling: serializing the objects
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+# create our training data
+training = []
+# create an empty array for our output
+output_empty = [0] * len(classes)
+
+# training set, bag of words (bow) for each sentence
+for doc in documents:
+    # initialize our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # lemmatize each word - create base word, in attempt to represent related words
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    # create our bag of words array with 1, if word match found in current pattern
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+
+    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    # print("training data: ",[bag, output_row]"\n")
+    training.append([bag, output_row])
+
+# checking results by printing: while needed you can uncomment print lines
+# print(" output: ",output_empty)
